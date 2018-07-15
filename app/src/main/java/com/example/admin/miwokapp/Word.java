39a86d3c8f5c3979f9dc17ce7dcb38114b5f31e8
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-15 20:40:18 +0530

    added getter method for imageId

--- app/src/main/java/com/example/admin/miwokapp/Word.java
+++ app/src/main/java/com/example/admin/miwokapp/Word.java
@@ -6,10 +6,18 @@ public class Word {
 
     /** Miwok translation for the word */
     private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
     /**
      * Get the default translation of the word.
@@ -24,4 +32,11 @@ public class Word {
         return mMiwokTranslation;
     }
 
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+    public boolean hasImage()
+    {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
 }
