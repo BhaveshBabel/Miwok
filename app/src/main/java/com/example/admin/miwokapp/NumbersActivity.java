ce879675c64ad7b844cdf5b79410bcda7d66b85c
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-15 20:39:22 +0530

    added number icons to activity

--- app/src/main/java/com/example/admin/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/example/admin/miwokapp/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.miwokapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.admin.miwokapp.R;
@@ -14,41 +15,26 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        //setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         // Create a list of words
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti",R.drawable.number_one));
+        words.add(new Word("two", "otiiko",R.drawable.number_two));
+        words.add(new Word("three", "tolookosu",R.drawable.number_three));
+        words.add(new Word("four", "oyyisa",R.drawable.number_four));
+        words.add(new Word("five", "massokka",R.drawable.number_five));
+        words.add(new Word("six", "temmokka",R.drawable.number_six));
+        words.add(new Word("seven", "kenekaku",R.drawable.number_seven));
+        words.add(new Word("eight", "kawinta",R.drawable.number_eight));
+        words.add(new Word("nine", "wo’e",R.drawable.number_nine));
+        words.add(new Word("ten", "na’aacha",R.drawable.number_ten));
 
-        // Find the root view so we can add child views to it
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
-        // Create a variable to keep track of the current index position
-        int index = 0;
-
-        // Keep looping until we've reached the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list)
-        while (index < words.size()) {
-            // Create a new TextView
-            TextView wordView = new TextView(this);
-
-            // Set the text to be word at the current index
-            wordView.setText(words.get(index));
-
-            // Add this TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-
-            // Increment the index variable by 1
-            index++;
-        }
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words,R.color.category_numbers);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
