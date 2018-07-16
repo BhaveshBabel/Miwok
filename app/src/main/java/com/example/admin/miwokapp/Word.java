24beb4f2461d6196307edfa03cf270719384e073
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-16 22:02:20 +0530

    Added getter method for AudioResourceId

--- app/src/main/java/com/example/admin/miwokapp/Word.java
+++ app/src/main/java/com/example/admin/miwokapp/Word.java
@@ -8,16 +8,19 @@ public class Word {
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int AudioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = AudioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int AudioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = AudioResourceId;
     }
     /**
      * Get the default translation of the word.
@@ -38,5 +41,8 @@ public class Word {
     public boolean hasImage()
     {
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+    public int getAudioResourceId(){
+        return mAudioResourceId;
     }
 }
